e47c39e326e6dab256bb3c396de2dbb15f78850f
0BVer <dr0joon@gmail.com>
2022-10-29 03:37:26 +0900

    feat : 1325 sol

--- src/dfs/q1325/Main.java
+++ src/dfs/q1325/Main.java
@@ -1,75 +1,59 @@
 package dfs.q1325;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
+//import java.io.BufferedWriter;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.*;
+//import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
+    //    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringBuilder result = new StringBuilder();
+    static ArrayList<Integer>[] graph = new ArrayList[10001];
+    static int[] counts = new int[10001];
+    static boolean[] visited = new boolean[10001];
+    static int size, link;
+
     public static void main(String[] args) throws Exception {
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int size = Integer.parseInt(st.nextToken());
-        int link = Integer.parseInt(st.nextToken());
+        size = Integer.parseInt(st.nextToken());
+        link = Integer.parseInt(st.nextToken());
 
         // 그래프 생성, 단방향 그래프
-        Map<Integer, List<Integer>> graph = new HashMap<>(size);
+        for (int i = 1; i <= size; i++) graph[i] = new ArrayList<>(10001);
         for (int i = 0; i < link; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            if (!graph.containsKey(b)) {
-                graph.put(b, new LinkedList<>());
-            }
-            graph.get(b).add(a);
+            graph[b].add(a);
         }
         br.close();
-
-        // dfs
-        int max = -1;
-        Set<Integer> visited = new HashSet<>(size);
-        StringBuilder result = new StringBuilder();
-        for (var i : graph.keySet()) {
-            int count = 1;
-            Queue<Integer> queue = new LinkedList<>();
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 1; i <= size; i++) {
+            visited = new boolean[10001];
+            visited[i] = true;
             queue.add(i);
-            visited.add(i);
             while (!queue.isEmpty()) {
-                int now = queue.poll();
-                count++;
-                if (graph.containsKey(now)) {
-                    graph.get(now).forEach(n -> {
-                        if (!visited.contains(n)) {
-                            visited.add(n);
-                            queue.add(n);
-                        }
-                    });
+                int temp = queue.poll();
+                for (int j = 0; j < graph[temp].size(); j++) {
+                    if (!visited[j]) {
+                        visited[j] = true;
+                        queue.add(j);
+                        counts[i]++;
+                    }
                 }
             }
-//            dfs(i, graph, visited);
-//            int count = visited.size();
-            if (count > max) {
-                max = count;
-                result = new StringBuilder();
-                result.append(i).append(" ");
-            } else if (count == max) result.append(i).append(" ");
-            visited.clear();
         }
-        bw.write(result.toString());
-        bw.flush();
-        bw.close();
-    }
-
-    static void dfs(int node, Map<Integer, List<Integer>> graph, Set<Integer> visited) {
-        if (graph.containsKey(node))
-            graph.get(node).forEach(n -> {
-                if (!visited.contains(n)) {
-                    visited.add(n);
-                    dfs(n, graph, visited);
-                }
-            });
+        int max = -1;
+        for (int i = 1; i <= size; i++) max = Math.max(max, counts[i]);
+        for (int i = 1; i <= size; i++) if (counts[i] == max) result.append(i + " ");
+//        bw.write(result.toString());
+//        bw.flush();
+//        bw.close();
+        System.out.println(result);
     }
 }
